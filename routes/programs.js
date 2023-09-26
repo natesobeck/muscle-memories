f5cb1e15baeb84ec6721d08853485008df230130
Nate Sobeck <nsobeck910@gmail.com>
2023-09-26 09:58:11 -0400

    added isLoggedIn middleware and conditionally rendered delete button for reviews

--- routes/programs.js
+++ routes/programs.js
@@ -1,20 +1,21 @@
 import { Router } from 'express'
 import * as programsCtrl from '../controllers/programs.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 router.get('/', programsCtrl.index)
 router.get('/new', programsCtrl.new)
 router.get('/:programId', programsCtrl.show)
-router.get('/:programId/edit', programsCtrl.edit)
+router.get('/:programId/edit', isLoggedIn, programsCtrl.edit)
 router.post('/', programsCtrl.create)
-router.post('/:programId/workouts', programsCtrl.createWorkout)
-router.post('/:programId/workouts/:workoutId/exercises', programsCtrl.addExerciseToWorkout)
-router.post('/:programId/reviews', programsCtrl.createReview)
-router.put('/:programId', programsCtrl.update)
-router.delete('/:programId', programsCtrl.delete)
-router.delete('/:programId/reviews/:reviewId', deleteReview)
-router.delete('/:programId/workouts/:workoutId/exercises/:exerciseId', programsCtrl.deleteExerciseFromWorkout)
+router.post('/:programId/workouts', isLoggedIn, programsCtrl.createWorkout)
+router.post('/:programId/workouts/:workoutId/exercises', isLoggedIn, programsCtrl.addExerciseToWorkout)
+router.post('/:programId/reviews', isLoggedIn, programsCtrl.createReview)
+router.put('/:programId', isLoggedIn, programsCtrl.update)
+router.delete('/:programId', isLoggedIn, programsCtrl.delete)
+router.delete('/:programId/reviews/:reviewId', isLoggedIn, programsCtrl.deleteReview)
+router.delete('/:programId/workouts/:workoutId/exercises/:exerciseId', isLoggedIn, programsCtrl.deleteExerciseFromWorkout)
 
 export {
   router
