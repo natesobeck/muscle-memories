f8620bf7e83e9fe01852a78c524f132e59f2ceea
Nate Sobeck <nsobeck910@gmail.com>
2023-09-27 17:20:13 -0400

    refine styling for program details and index pages

--- routes/posts.js
+++ routes/posts.js
@@ -6,12 +6,12 @@ const router = Router()
 
 router.get('/', postsCtrl.index)
 router.get('/:postId', postsCtrl.show)
-router.post('/', postsCtrl.create)
-router.post('/:postId/comments/', postsCtrl.createComment)
-router.put('/:postId', postsCtrl.updatePost)
-router.put('/:postId/comments/:commentId', postsCtrl.updateComment)
-router.delete('/:postId', postsCtrl.delete)
-router.delete('/:postId/comments/:commentId', postsCtrl.deleteComment)
+router.post('/', isLoggedIn, postsCtrl.create)
+router.post('/:postId/comments/', isLoggedIn, postsCtrl.createComment)
+router.put('/:postId', isLoggedIn, postsCtrl.updatePost)
+router.put('/:postId/comments/:commentId', isLoggedIn, postsCtrl.updateComment)
+router.delete('/:postId', isLoggedIn, postsCtrl.delete)
+router.delete('/:postId/comments/:commentId', isLoggedIn, postsCtrl.deleteComment)
 
 export {
   router
